b718fec64690b76382c1c86530a061c871706561
Anthony Cullather <anthonycullather.jr@gmail.com>
2014-06-09 15:31:28 -0500

    add keyboard events

--- SFML_test/Player.h
+++ SFML_test/Player.h
@@ -11,24 +11,25 @@ public:
 	~Player();
 
 	void AddSprite( std::string filename, sf::Texture* texture );
-	void			AddSprite(sf::Sprite sprite);
+	void AddSprite( sf::Sprite sprite );
+	void Dispose( void );
+	sf::Sprite GetCurrentSprite( void );
+	int	GetCurrentSpriteIndex( void );
+	void SetCurrentSpriteIndex( int index );
 	void SetPosition( float x, float y );
 	void SetRotation( float angle );
-	void			Dispose( void );
-	sf::Sprite		GetCurrentSprite( void );
-	int				GetCurrentSpriteIndex( void );
-	void			SetCurrentSpriteIndex( int index );
-	void			RemoveSprite( int index );
-	void			RemoveSprite( std::string filename );
+	void RemoveSprite( int index );
+	void RemoveSprite( std::string filename );
 
 private:
-	float						m_posX;
-	float						m_posY;
-	int						m_currentSpriteIndex;
+	float m_posX;
+	float m_posY;
+	int	 m_currentSpriteIndex;
+	sf::Vector2f m_velocity;
 	
-	sf::Image				m_image;
-	sf::Texture				m_texture;
-	sf::Sprite				m_currentSprite;
+	sf::Image m_image;
+	sf::Texture	m_texture;
+	sf::Sprite	m_currentSprite;
 	std::vector<sf::Sprite> m_spriteArray;
 	std::vector<sf::Texture> m_textureArray;
 };
--- SFML_test/main.cpp
+++ SFML_test/main.cpp
@@ -12,6 +12,7 @@ const std::string IMAGE_PATH_MAN4 = "c:\\dev\\images\\man4.png";
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 const int MAX_IMAGES = 4;
+const float MAXIMUM_VELOCITY = 4.0f;
 
 const float FRAME_RATE = 1/20.0f;
 
@@ -23,9 +24,11 @@ int main()
 	sf::Time animTimer = time1;
 
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My Game Window");
+	window.setFramerateLimit(60);
 
 	Player player;
 	float posX = 0.0f, posY = 0.0f, angle = 0.0f;
+	float currentVelocity = 0.0f;
 	sf::Texture t1, t2, t3, t4;
 	player.AddSprite(IMAGE_PATH_MAN1, &t1);
 	player.AddSprite(IMAGE_PATH_MAN2, &t2);
@@ -43,41 +46,65 @@ int main()
 			{
 				window.close();
 			}
+
+			if ( event.type == sf::Event::KeyPressed )
+			{
+				switch ( event.key.code )
+				{
+					case sf::Keyboard::Escape:
+						window.close();
+						break;
+					case sf::Keyboard::Left:
+						posX -= MAXIMUM_VELOCITY;
+						if ( posX < 0.0f )
+						{
+							posX = WINDOW_WIDTH;
+						}
+						break;
+					case sf::Keyboard::Right:
+						posX += MAXIMUM_VELOCITY;
+						if ( posX > WINDOW_WIDTH )
+						{
+							posX = 0.0;
+						}
+						break;
+					case sf::Keyboard::Up:
+						posY -= MAXIMUM_VELOCITY;
+						if ( posY < 0.0f )
+						{
+							posY = WINDOW_HEIGHT;
+						}
+						break;
+					case sf::Keyboard::Down:
+						posY += MAXIMUM_VELOCITY;
+						if ( posY > WINDOW_HEIGHT )
+						{
+							posY = 0.0f;
+						}
+						break;
+					default:
+						break;
+				}
+			}
 		}
 
 		time2 = clock.getElapsedTime();
-		if ( (time2.asSeconds() - time1.asSeconds()) >= FRAME_RATE )
+		if ( (time2.asSeconds() - animTimer.asSeconds()) >= FRAME_RATE * 2 )
 		{
-			if ( (time2.asSeconds() - animTimer.asSeconds()) >= (FRAME_RATE * 4) )
+			currentImageIndex = player.GetCurrentSpriteIndex();
+			currentImageIndex++;
+			if ( currentImageIndex > ( MAX_IMAGES - 1 ))
 			{
-				currentImageIndex = player.GetCurrentSpriteIndex();
-				currentImageIndex++;
-				if ( currentImageIndex > ( MAX_IMAGES - 1 ))
-				{
-					player.SetCurrentSpriteIndex( 0 );
-				}
-				else
-				{
-					player.SetCurrentSpriteIndex( currentImageIndex );
-				}
-				animTimer = time2;
+				player.SetCurrentSpriteIndex( 0 );
 			}
-
-			posX += 4; 
-			posY += 4;
-			srand(10);
-			angle += rand();
-			if ( posX >= WINDOW_WIDTH - t1.getSize().x )
+			else
 			{
-				posX = 0;
+				player.SetCurrentSpriteIndex( currentImageIndex );
 			}
-			if ( posY >= WINDOW_HEIGHT - t1.getSize().y )
-			{
-				posY = 0;
-			}
-			player.SetPosition( posX, posY );
-			time1 = time2;
+			animTimer = time2;
 		}
+
+		player.SetPosition( posX, posY );
 
 		window.clear();
 		window.draw( player.GetCurrentSprite() );
